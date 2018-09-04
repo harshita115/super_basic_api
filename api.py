7d2c42f385b292631fb74c6232b29abf949b8fb9
abhimanyu121 <abhimanyushekhawat17.as@gmail.com>
2018-09-04 23:46:27 +0530

    very first adn very basic api

---
+++ api.py
@@ -0,0 +1,34 @@
+import flask 
+from flask import request,jsonify
+app = flask.Flask(__name__)
+app.config["DEBUG"]=True
+@app.route('/',methods=['get'])
+def home():
+	return "<h1>Distant reading api</h1><p>this site is prototype for distant reading api</p>"
+books=[
+ {
+  'id':0,
+  'val':12
+ },
+ {
+ 'id':1,
+ 'value':15
+ }
+]
+@app.route('/ab',methods=['get'])
+def random():
+	#checking if any id is passed with the link
+	#The id must be provided like this: linl/books?id=0
+	#and if id is provided i am gonna assign it to a variable 
+   if 'id' in request.args:
+        id = int(request.args['id'])
+   else:
+        return "Error: No id field provided. Please specify an id."
+   results=[]
+   #empty list of results
+   for book in books:
+   	 if book['id']==id:
+   		 results.append(book)
+   return jsonify(results)
+
+app.run()
